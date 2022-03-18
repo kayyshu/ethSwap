0a6b276b97ba297eff6e8c3f7f469698d8328dfb
Naman Mishra <kayyshu@gmail.com>
2022-03-18 09:43:19 +0530

    Final contract

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -8,7 +8,7 @@ contract EthSwap {
   Token public token;
   uint public rate = 100;
 
-  event TokenPurchased(
+  event TokensPurchased(
       address account,
       address token,
       uint amount,
@@ -35,12 +35,18 @@ contract EthSwap {
     // Transfer tokens to the user
     token.transfer(msg.sender, tokenAmount);
 
-    emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+    emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
   }
 
   function sellTokens(uint _amount) public{
+    //Users can't sell more tokens than they have
+    require(token.balanceOf(msg.sender) == _amount);
+
     //Calculate the amount of Ether to redeem
     uint etherAmount = _amount/ rate;
+
+    //Require that EthSwap has enough Ether
+    require(address(this).balance >= etherAmount);
 
     //Perform sale
     token.transferFrom(msg.sender, address(this), _amount);
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -86,6 +86,9 @@ contract('EthSwap', ([deployer, investor]) =>{
 		assert.equal(event.token, token.address)
 		assert.equal(event.amount.toString(), tokens('100').toString())
 		assert.equal(event.rate.toString(), '100')
+
+	    //FAILURE: Investor cant sell more tokens than they have
+	    await ethSwap.sellTokens(tokens('500'),{ from: investor}).should.be.rejected;
 		})
 	})
 })
